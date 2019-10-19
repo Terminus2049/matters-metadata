3b74ffb4edf97fae7e671aa183f76754a7d51032
terminusbot <terminus2049@protonmail.com>
2019-10-20 02:20:58 +0800

    matters 接口又变更

--- update_newest.R
+++ update_newest.R
@@ -1,71 +1,45 @@
-library(httr)
-library(jsonlite)
-library(readr)
+require(httr)
+require(jsonlite)
 
 url <- "https://server.matters.news/graphql/"
-GQL <- function(query, .url = url){
-  pbody <- list(query = query)
-  res <- POST(.url, body = pbody, encode="json")
-  res <- content(res, as = "parsed", encoding = "UTF-8")
-  if(!is.null(res$errors)){
-    warning(toJSON(res$errors))
-  }
-  res$data
-}
 
-article_query <- '{
-  viewer {
-    recommendation {
-      feed: newest(input: {first: 30}) {
-        edges {
-    			node {
-              id
-  						title
-  						slug
-  						createdAt
-  						state
-  						public
-  						live
-  						summary
-  						mediaHash
-  						wordCount
-  						dataHash
-  						mediaHash
-  						MAT
-  						commentCount
-  						responseCount
-              author {
-              	id
-              	userName
-              	displayName
-              }
-          	}
-          }
-        }
-      }
-    }
-  }
-'
+body <- list(operationName = 'NewestFeed', 
+             variables = list(hasArticleDigestActionAuthor = F,
+                              hasArticleDigestActionBookmark = T,
+                              hasArticleDigestActionTopicScore = F),
+             extensions = list(persistedQuery = list(version = 1,
+                                                     sha256Hash = '08291b314126ef9011592f1b04652a6960fa01a5de68965451c7552188381774')),
+             query = 'query NewestFeed($after: String, $hasArticleDigestActionAuthor: Boolean = false, $hasArticleDigestActionBookmark: Boolean = true, $hasArticleDigestActionTopicScore: Boolean = false) {\n  viewer {\n    id\n    recommendation {\n      feed: newest(input: {first: 10, after: $after}) {\n        ...FeedArticleConnection\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment FeedArticleConnection on ArticleConnection {\n  pageInfo {\n    startCursor\n    endCursor\n    hasNextPage\n    __typename\n  }\n  edges {\n    cursor\n    node {\n      ...FeedDigestArticle\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment FeedDigestArticle on Article {\n  id\n  title\n  slug\n  cover\n  summary\n  mediaHash\n  live\n  author {\n    id\n    userName\n    ...UserDigestMiniUser\n    __typename\n  }\n  ...DigestActionsArticle\n  ...FingerprintArticle\n  ...DropdownActionsArticle\n  __typename\n}\n\nfragment UserDigestMiniUser on User {\n  id\n  userName\n  displayName\n  ...AvatarUser\n  __typename\n}\n\nfragment AvatarUser on User {\n  avatar\n  __typename\n}\n\nfragment DigestActionsArticle on Article {\n  author {\n    ...UserDigestMiniUser @include(if: $hasArticleDigestActionAuthor)\n    __typename\n  }\n  createdAt\n  ...AppreciationArticle\n  ...ResponseCountArticle\n  ...BookmarkArticle @include(if: $hasArticleDigestActionBookmark)\n  ...TopicScoreArticle @include(if: $hasArticleDigestActionTopicScore)\n  ...StateActionsArticle\n  __typename\n}\n\nfragment AppreciationArticle on Article {\n  appreciationsReceivedTotal\n  __typename\n}\n\nfragment ResponseCountArticle on Article {\n  id\n  slug\n  mediaHash\n  responseCount\n  author {\n    userName\n    __typename\n  }\n  __typename\n}\n\nfragment BookmarkArticle on Article {\n  id\n  subscribed\n  __typename\n}\n\nfragment TopicScoreArticle on Article {\n  topicScore\n  __typename\n}\n\nfragment StateActionsArticle on Article {\n  state\n  __typename\n}\n\nfragment FingerprintArticle on Article {\n  id\n  dataHash\n  __typename\n}\n\nfragment DropdownActionsArticle on Article {\n  id\n  ...ArchiveButtonArticle\n  ...StickyButtonArticle\n  __typename\n}\n\nfragment StickyButtonArticle on Article {\n  id\n  sticky\n  author {\n    id\n    userName\n    __typename\n  }\n  __typename\n}\n\nfragment ArchiveButtonArticle on Article {\n  id\n  state\n  author {\n    id\n    userName\n    __typename\n  }\n  __typename\n}\n')
 
-newest = GQL(article_query)
-newest_list = newest$viewer$recommendation$feed$edges
+res <- POST(url, body = body, encode = "json")
+res <- content(res, as = "parsed", encoding = "UTF-8")
+
+newest_list = res$data$viewer$recommendation$feed$edges
 
 article = newest_list[[1]]$node
-article = as.data.frame(unlist(c(article[1:14],article$author$id,
-                                 article$author$userName, 
-                                 article$author$displayName)))
-
+article = c(article[1:3],article[5:16])
+article = as.data.frame(unlist(article[1:14]))
 for (i in 2:length(newest_list)) {
   print(i)
   x = newest_list[[i]]$node
-  article = cbind(article, as.data.frame(unlist(c(x[1:14],x$author$id, 
-                                                  x$author$userName, 
-                                                  x$author$displayName))))
+  x = c(x[1:3],x[5:16])
+  df = as.data.frame(unlist(x[1:14]))
+  article = cbind(article, df)
 }
 
 article = t(article)
 row.names(article) = NULL
 article = as.data.frame(article)
+article$public = NA
+article$wordCount = NA
+article$MAT = NA
+article$commentCount = NA
+article$responseCount = NA
+
+
+
+article = article[,c(1,2,3,12,17,19,6,4,5,20,18,21,22,15,7,8,9)]
+
 
 colnames(article) = c('id', 'title', 'slug', 'createdAt', 'state',
                       'public', 'live', 'summary', 'mediaHash', 
@@ -78,3 +52,5 @@ NewestFeed <- read_csv("csv/NewestFeed.csv")
 NewestFeed <- unique(rbind(NewestFeed,article))
 NewestFeed = NewestFeed[!duplicated(NewestFeed$dataHash), ]
 write_csv(NewestFeed, "csv/NewestFeed.csv")
+
+
